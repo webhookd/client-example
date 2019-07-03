a7ef94713c9f236cb219323e0fe04604cbadd0e4
Brooke Bryan <brooke@bajb.net>
2019-07-03 16:10:09 +0100

    Send off every 30 seconds

--- main.go
+++ main.go
@@ -21,16 +21,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	for {
 
-	msg := webhookMessage{Name: "test message", Time: time.Now(), Uuid: uuid.NewV4().String()}
-	jsnBody, err := json.Marshal(msg)
+		msg := webhookMessage{Name: "test message", Time: time.Now(), Uuid: uuid.NewV4().String()}
+		jsnBody, err := json.Marshal(msg)
 
-	webhooks := webhookd.NewWebhookdApiClient(webhookdConnection)
-	resp, err := webhooks.Transmit(context.Background(), &webhookd.TransmitRequest{
-		Provider:     &webhookd.Provider{Id: "provider-id"},
-		Subscription: &webhookd.Subscription{Key: "provider-set-subscription-key"},
-		Message: &webhookd.Message{
-			Method: webhookd.HttpMethod_POST,
+		webhooks := webhookd.NewWebhookdApiClient(webhookdConnection)
+		provider := &webhookd.Provider{Id: "chargehive"}
+
+		subscription := &webhookd.Subscription{}
+		subscription.Key = "testing"
+
+		message := &webhookd.Message{
+			Id:     "example-id",
+			Method: webhookd.HTTP_METHOD_POST,
 			Body:   string(jsnBody),
 			Query: map[string]string{
 				"token": "accessToken",
@@ -39,7 +43,38 @@ func main() {
 				"x-example-url": "http://originated.at.localhost",
 				"content-type":  "application/json",
 			},
-		},
-	})
-	log.Print(resp, err)
+		}
+
+		if subscription.Id == "" && subscription.Key == "" {
+			invResp, err := webhooks.SubscriptionCreate(context.Background(), &webhookd.SubscriptionCreateRequest{
+				Provider: provider,
+				Key:      "client-test" + time.Now().Format("Jan-2-15-04-05"),
+				Name:     "Client Test " + time.Now().Format(time.RFC822),
+			})
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			subscription = invResp.Subscription
+
+			accept, err := webhooks.SubscriptionAccept(context.Background(), &webhookd.SubscriptionAcceptRequest{
+				Subscription: subscription,
+				Consumer:     &webhookd.Consumer{Id: "random-consumer-id"},
+				Token:        invResp.Token,
+			})
+
+			if err != nil || accept.Received {
+				log.Fatal(err, accept)
+			}
+		}
+
+		resp, err := webhooks.Transmit(context.Background(), &webhookd.TransmitRequest{
+			Provider:     provider,
+			Subscription: subscription,
+			Message:      message,
+		})
+		log.Print(resp, err)
+		time.Sleep(time.Second * 20)
+	}
 }
